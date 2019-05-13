0ffecd198785f0e56e08472b67980b5665efdc33
Frebby <fredudoh33@gmail.com>
2019-05-13 12:43:22 +0100

    merging with sammy

--- app/src/main/java/com/primedsoft/primedpoll/Activities/AddTextPoll.java
+++ app/src/main/java/com/primedsoft/primedpoll/Activities/AddTextPoll.java
@@ -17,6 +17,7 @@ import com.primedsoft.primedpoll.R;
  * A simple {@link Fragment} subclass.
  */
 public class AddTextPoll extends Fragment {
+    int nextCount = 4;
 
 
     public AddTextPoll() {
@@ -25,28 +26,38 @@ public class AddTextPoll extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.add_text_poll, container, false);
         AppCompatButton next = view.findViewById(R.id.next_button);
+        AppCompatButton cancel = view.findViewById(R.id.cancel_button);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v = inflater.inflate(R.layout.activity_create_poll,container, false);
+            }
+        });
         final EditText textPoll = view.findViewById(R.id.text_poll_edit);
+
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int nextCount = 4;
 
                 if(textPoll.getText() != null){
                     --nextCount;
                     Toast.makeText(getContext(), "Polls remaining is " + nextCount, Toast.LENGTH_SHORT).show();
                     textPoll.setText("");
-                }else{
+
+                }else {
+                    textPoll.getText();
                     Toast.makeText(getContext(), "Poll cant be empty " + nextCount, Toast.LENGTH_SHORT).show();
 
                 }
                 if(nextCount < 0){
                     Toast.makeText(getContext(), "Poll limit reached " + nextCount, Toast.LENGTH_SHORT).show();
-
+nextCount = 0;
                 }
+
             }
         });
         return view;
@@ -55,6 +66,4 @@ public class AddTextPoll extends Fragment {
     public static AddTextPoll newInstance() {
         return new AddTextPoll();
     }
-
-
 }
